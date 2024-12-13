584d48606ee80225e385b1dd6f2e8ec6464e814d
Michael Maier <mm263@hdm-stuttgart.de>
2024-12-13 12:18:14 +0100

    jumping implemented

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
 
     height = 280;
-    y = 155;
+    y = 80;
     speed = 4;
     IMAGES_WALKING = [
         "img/2_character_pepe/2_walk/W-21.png",
@@ -11,43 +11,64 @@ class Character extends MovableObject {
         "img/2_character_pepe/2_walk/W-25.png",
         "img/2_character_pepe/2_walk/W-26.png"
     ];
+
+    IMAGES_JUMPING = [
+        "img/2_character_pepe/3_jump/J-31.png",
+        "img/2_character_pepe/3_jump/J-32.png",
+        "img/2_character_pepe/3_jump/J-33.png",
+        "img/2_character_pepe/3_jump/J-34.png",
+        "img/2_character_pepe/3_jump/J-35.png",
+        "img/2_character_pepe/3_jump/J-36.png",
+        "img/2_character_pepe/3_jump/J-37.png",
+        "img/2_character_pepe/3_jump/J-38.png",
+        "img/2_character_pepe/3_jump/J-39.png"
+    ];
+
     world;
     walking_sound = new Audio('./audio/running.mp3');
 
     constructor() {
         super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
 
         this.animate();
+        this.applyGravity();
     }
 
-    jump() {
 
-    }
 
     animate() {
 
         setInterval(() => {
             this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.x += this.speed;
+                this.moveRight();
                 this.otherDirection = false;
                 this.walking_sound.play();
             }
 
-            if (this.world.keyboard.LEFT && this.x > 0 ) {
-                this.x -= this.speed;
+            if (this.world.keyboard.LEFT && this.x > 0) {
+                this.moveLeft();
                 this.otherDirection = true;
                 this.walking_sound.play();
             }
-            this.world.camera_x = -this.x + 100;
-        }, 1000/60);
 
-        setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-              this.playAnimation(this.IMAGES_WALKING);
+            if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+                this.jump();
             }
 
+            this.world.camera_x = -this.x + 100;
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
+            }
         }, 50);
 
     }
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -8,6 +8,8 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2;
 
     loadImage(path) {
         this.img = new Image();
@@ -22,21 +24,36 @@ class MovableObject {
         });
     }
 
+    jump() {
+        this.speedY = 30;
+    }
+
     moveRight() {
-
+        this.x += this.speed;
     }
 
-    moveLeft(){
-        setInterval(()=>{
-            this.x -= this.speed;
-        }, 1000/60);
+    moveLeft() {
+        this.x -= this.speed;
     }
 
-    playAnimation(images){
-          //Walk Animation
-          let i = this.currentImage % images.length // Modulo ist der Rest also: i = 0/6 => Rest 0, 1/6 => Rest 1, ... also 0,1,2,3,4,5, 0,1,2,3,...
-          let path = images[i];
-          this.img = this.imageCache[path];
-          this.currentImage++;
+    playAnimation(images) {
+        //Walk Animation
+        let i = this.currentImage % images.length // Modulo ist der Rest also: i = 0/6 => Rest 0, 1/6 => Rest 1, ... also 0,1,2,3,4,5, 0,1,2,3,...
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25)
+    }
+
+    isAboveGround() {
+        return this.y < 150;
     }
 }
